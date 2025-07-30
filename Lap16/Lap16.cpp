753e68d0e77b21bf42c6bbadb78c92430d01cbec
intel <intel@DESKTOP-JUF9Q2S>
2025-07-30 03:37:22 +0300

    Add project files.

---
+++ Lap16/Lap16.cpp
@@ -0,0 +1,40 @@
+// Lap16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, b ,choice;
+    cout << "Enter two number";
+    cin >> a >> b;
+    choice = (a > b) ? 1 : ((b > a) ? 2 : 3);
+    
+        switch (choice){
+        case 1:
+            cout << "maximum is" << a;
+            break;
+        case 2:
+            cout << "maximum is " << b;
+            break;
+        case 3:
+            cout << "both number are equal";
+            break;
+    }
+    return 0;
+            
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
